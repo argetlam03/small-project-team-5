0a931b051bc29c1e071a8fdcbf12ca5a6e3b371d
CadeO3712 <centralflcade@gmail.com>
2024-09-11 19:47:57 -0400

    Added DeleteContact.php

---
+++ LAMPAPI/DeleteContact.php
@@ -0,0 +1,39 @@
+<?php
+	$inData = getRequestInfo();
+	
+	$userID = $inData["userId"];
+	$id = $inData["id"];
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("DELETE FROM Contacts WHERE userId = ? AND id = ?");
+		$stmt->bind_param("ss", $userId, $id);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("Contact Deleted");
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
