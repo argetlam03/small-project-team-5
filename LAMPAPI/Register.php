2b2cc650b24402d09cf8c875fc404bd4ed1a8cde
Sathvik Kuthuru <31018656+SathvikKuthuru@users.noreply.github.com>
2024-09-12 04:03:41 -0400

    Create Register.php

---
+++ LAMPAPI/Register.php
@@ -0,0 +1,61 @@
+<?php
+
+    $inData = getRequestInfo();
+    
+    $firstName = $inData["firstName"];
+    $lastName = $inData["lastName"];
+    $login = $inData["login"];
+    $password = $inData["password"];
+
+    $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+    
+    if ($conn->connect_error) {
+        returnWithError($conn->connect_error);
+    } else {
+        $stmt = $conn->prepare("SELECT ID FROM Users WHERE Login=?");
+        $stmt->bind_param("s", $login);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        if ($result->num_rows > 0) {
+            returnWithError("Login already exists");
+        } else {
+            $stmt = $conn->prepare("INSERT INTO Users (FirstName, LastName, Login, Password) VALUES (?, ?, ?, ?)");
+            $stmt->bind_param("ssss", $firstName, $lastName, $login, $password);
+            
+            if ($stmt->execute()) {
+                $id = $stmt->insert_id;
+                returnWithInfo($firstName, $lastName, $id);
+            } else {
+                returnWithError("Failed to register user");
+            }
+        }
+
+        $stmt->close();
+        $conn->close();
+    }
+
+    function getRequestInfo()
+    {
+        return json_decode(file_get_contents('php://input'), true);
+    }
+
+    function sendResultInfoAsJson($obj)
+    {
+        header('Content-type: application/json');
+        echo $obj;
+    }
+    
+    function returnWithError($err)
+    {
+        $retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+        sendResultInfoAsJson($retValue);
+    }
+    
+    function returnWithInfo($firstName, $lastName, $id)
+    {
+        $retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
+        sendResultInfoAsJson($retValue);
+    }
+
+?>
