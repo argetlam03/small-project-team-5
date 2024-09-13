f12b0f5ff066f6ee9235951d2b9ec7a6ea22549b
parhamdevin <parhamd08@gmail.com>
2024-09-13 15:52:55 -0400

    fixed search with contact id

--- LAMPAPI/SearchContacts.php
+++ LAMPAPI/SearchContacts.php
@@ -12,11 +12,11 @@
 	} 
 	else
 	{
-		$stmt = $conn->prepare("SELECT * FROM Contacts WHERE Name LIKE ? and Phone LIKE ? and Email LIKE ? and UserID = ? and ID = ?");
+		$stmt = $conn->prepare("SELECT * FROM Contacts WHERE Name LIKE ? and Phone LIKE ? and Email LIKE ? and UserID = ?");
 		$Name = "%" . $inData["name"] . "%";
 		$Phone = "%" . $inData["phone"] . "%";
 		$Email = "%" . $inData["email"] . "%";
-		$stmt->bind_param("sssss", $Name, $Phone, $Email, $inData["userId"],$inData["ID"]);
+		$stmt->bind_param("ssss", $Name, $Phone, $Email, $inData["userId"]);
 		$stmt->execute();
 		
 		$result = $stmt->get_result();
